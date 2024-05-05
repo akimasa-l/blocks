e14a0aa4eefb5d7c9f1a8b07dc1a3f882bddd994
akimasa-l <liumingzheng8@outlook.com>
2024-05-05 12:58:07 +0900

    Initial Commit

---
+++ blocks/blocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  blocksApp.swift
+//  blocks
+//
+//  Created by 劉明正 on 2024/05/05.
+//
+
+import SwiftUI
+
+@main
+struct blocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
