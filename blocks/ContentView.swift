264b2ded72d17e09275c5cde2a7269037a76e2c6
akimasa-l <liumingzheng8@outlook.com>
2024-05-06 10:48:15 +0900

    いいかも

--- blocks/ContentView.swift
+++ blocks/ContentView.swift
@@ -9,30 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Grid{
-            GridRow{
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-            }
-            GridRow{
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-            }
-            GridRow{
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
-                Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/,height: 20).foregroundColor(.blue)
+        GeometryReader{ geometry in
+            let column = min(3, Int(floor(geometry.size.height/25)))
+            let row = Int(floor(geometry.size.width/105))
+            let x_space = (geometry.size.width - CGFloat(row * 105 - 5))/2
+            
+            VStack{
+                Grid(horizontalSpacing: 5, verticalSpacing: 5){
+                    ForEach(0..<column,id: \.self){ _ in
+                        GridRow{
+                            ForEach(0..<row,id: \.self){ _ in
+                                Rectangle().frame(width: 100,height: 20).foregroundColor(.blue)
+                            }
+                        }
+                    }
+                }.padding([.leading, .trailing], x_space)
+                Text("width: \(geometry.size.width), height: \(geometry.size.height)")
             }
         }
     }
